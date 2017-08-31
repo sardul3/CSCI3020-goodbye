3fd4bb7dfa6210cb7f60fab6943be5c938d4b50d
poudels <poudelsagar530@gmail.com>
2017-08-31 14:26:02 -0500

    changes comment added

--- HelloCSCI3020/src/HelloCSCI3020.java
+++ HelloCSCI3020/src/HelloCSCI3020.java
@@ -4,6 +4,7 @@ public class HelloCSCI3020
 
 	public static void main(String[] args)
 	{
+		// Changes 
 		System.out.println("Hello CSCI 3020 ;)!");
 		Simple msg = new Simple("Goodbye");
 		System.out.println(msg);
