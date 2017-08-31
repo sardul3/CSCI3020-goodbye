2832288b3c064098311654944d1fb69da651f1bf
poudels <poudelsagar530@gmail.com>
2017-08-31 14:37:35 -0500

    removed the emoticon

--- HelloCSCI3020/src/HelloCSCI3020.java
+++ HelloCSCI3020/src/HelloCSCI3020.java
@@ -5,7 +5,7 @@ public class HelloCSCI3020
 	public static void main(String[] args)
 	{
 		// Changes 
-		System.out.println("Hello CSCI 3020 ;)!");
+		System.out.println("Hello CSCI 3020 !");
 		Simple msg = new Simple("Goodbye");
 		System.out.println(msg);
 		// second comment
